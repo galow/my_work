17f93b2e7c5a2902415831af90b70aad07f6f8e8
galow-win <galow@qq.com>
2013-05-05 15:36:28 -0700

    add files

---
+++ hw6.cpp
@@ -0,0 +1,14 @@
+/**this file goes to solve 2-sat problem***/
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2){
+    std::cerr<<"Usage: "<<argv[0]<<"  "<<std::endl;
+    exit(EXIT_FAILURE);
+  }
+  
+  return 0;
+}
